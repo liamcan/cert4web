f70ada00a2037b376a6b2b8a17e5a2b9715ceddf
Adam Clarke <adamc@strategicdata.com.au>
2019-06-28 15:29:42 +0800

    Tidy and remove errors

--- functions.js
+++ functions.js
@@ -1,33 +1,5 @@
 
-// ONFOCUS FUNCTIONS//
-
-function focusName ()
-{
-		document.getElementById("nameinput").style.background="white";  /*BG color change fucntion for nameinput*/
-		}
-function unFocusName ()
-{
-		document.getElementById("nameinput").style.background="#E1E1E1";  /*BG color change fucntion for nameinput*/
-		}
-function focusEmail ()
-{
-		document.getElementById("emailinput").style.background="white";  /*BG color change fucntion for emailinput*/
-		}
-function unFocusEmail ()
-{
-		document.getElementById("emailinput").style.background="#E1E1E1";  /*BG color change fucntion for emailinput*/
-		}
-function focusComment ()
-{
-	document.getElementById("notes").style.background="white";				/*BG color change function for comments*/
-}
-function unFocusComment ()
-{
-	document.getElementById("notes").style.background="#E1E1E1";
-}
-
-
-// Image clicker function that cycles 3 images//
+// Image clicker code that cycles through images//
 
 var images = ["images/art.jpg", "images/coffee1.jpg", "images/coffee2.jpg"]
 
@@ -35,7 +7,7 @@ var imgState = 0;
 
 var imgTag = document.getElementById("imgClickAndChange");
 
-if ( imgTag ) {
+if (imgTag) {
   imgTag.addEventListener("click", function(event) {
     imgState = (++imgState % images.length);
     event.target.src = images[imgState];
